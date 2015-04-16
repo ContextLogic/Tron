84e0b5163e4a1ed30a6aa3515a4aef7d0f1257e9
David <david@contextlogic.com>
2015-04-16 16:20:32 -0700

    namespace js

--- tronweb/js/wish.js
+++ tronweb/js/wish.js
@@ -1,3 +1,3 @@
 setInterval(function(){
-  $( "a" ).each(function(i){if (this.innerHTML.indexOf('MASTER.')!=-1) {newHTML = this.innerHTML.replace('MASTER.',''); newHTML = newHTML.replace(/_/g, ' '); this.innerHTML = newHTML;}})
+  $( "a" ).each(function(i){if (!this.innerHTML){return} if (this.innerHTML.indexOf('MASTER.')!=-1) {newHTML = this.innerHTML.replace('MASTER.','');} else {newHTML = this.innerHTML.replace('.', ' - ');} newHTML = newHTML.replace(/_/g, ' '); this.innerHTML = newHTML;})
 }, 3000);
